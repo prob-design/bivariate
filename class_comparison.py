98349c5cf8928b5980722d15475537f9b729e034
Robert Lanzafame <lanzafame.robert@gmail.com>
2022-08-04 04:26:17 +0200

    briefly describe classes Comparison and Distribution

---
+++ class_comparison.py
@@ -0,0 +1,8 @@
+class Comparison():
+    """
+    Carry out a comparison of two or more Distribution objects, either as 
+    plots or calculated values. For example, plots of PDF's for several 
+    distributions. Tables include values of PDF's or CDF's, but also can 
+    compare moments, distribution parameters, etc.
+    """
+    pass
---
+++ class_distribution.py
@@ -0,0 +1,10 @@
+class Distribution():
+    """
+    Define a continuous distribution, with, at minimum methods f(x) and F(x).
+    
+    A Distribution can be defined in a variety of ways, using subclasses, for 
+    example: Scipy, OpenTURNS or custom function. Some distributions may be 
+    stored in a library (dictionary?), a summary of which can be printed by 
+    the Distribution class via a static method.
+    """
+    pass
