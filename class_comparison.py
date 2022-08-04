1edf5a552e988f427aae59e5c02997d6db1da50b
Robert Lanzafame <lanzafame.robert@gmail.com>
2022-08-04 06:10:28 +0200

    update class spec: Distribution and Comparison

--- class_comparison.py
+++ class_comparison.py
@@ -4,5 +4,24 @@ class Comparison():
     plots or calculated values. For example, plots of PDF's for several 
     distributions. Tables include values of PDF's or CDF's, but also can 
     compare moments, distribution parameters, etc.
+    
+    An instance of this class is created by providing a list of distributions 
+    (instances of the Distribution class).
+    
+    Start with plots, since that's in bivariate package already. 
+    Tables can come later
+    
     """
-    pass
+    def __init__(self, distributions):
+        """
+        Attributes will include some summary information about the 
+        distributions that will be compared, mostly filled in from 
+        distribution attributes.
+        """
+        pass
+    
+    def pdf():
+        pass
+    
+    def cdf():
+        pass
