24923040b6dec4f9e9f489e69a1b903add068b93
Caspar Jungbacker <caspar.jungbacker@outlook.com>
2022-08-16 20:56:42 +0200

    Make new README

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
     version='2.0.0',
     description='This package contains methods that assist in performing\
         bivariate analysis of datasets.',
-    url='https://gitlab.tudelft.nl/mude/bivariate_package',
+    url='https://gitlab.tudelft.nl/mude/bivariate_package.git',
     packages=find_packages(exclude=['old_package*']),
     install_requires=[
         'numpy',
