a3a4d96cbf5932972a43bdd9293a221e41b6d047
Benjamin Ramousse <b.ramousse@student.tudelft.nl>
2023-07-22 17:34:22 +0200

    disable import other classes than multivar

--- src/__init__.py
+++ src/__init__.py
@@ -1,3 +1,4 @@
-from . import class_dataset
-from . import class_distribution
-from . import class_comparison
+# from . import class_dataset
+# from . import class_distribution
+# from . import class_comparison
+from . import class_multivar
