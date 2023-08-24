83d9972dee7af25ae8da5957f7072da40bdc0cc9
bramousse <130069694+bramousse@users.noreply.github.com>
2023-08-24 19:43:00 +0200

    remove project info from setup.py

--- setup.py
+++ setup.py
@@ -1,23 +1,26 @@
 from setuptools import find_packages
 from setuptools import setup
 
-setup(
-    name='bivariate',
-    version='2.0.0',
-    description='This package contains methods that assist in performing\
-        bivariate analysis of datasets.',
-    url='https://github.com/prob-design/bivariate.git',
-    packages=find_packages(exclude=['old_package*']),
-    install_requires=[
-        'numpy',
-        'pandas',
-        'matplotlib >= 3.5.0',
-        'seaborn',
-        'scipy',
-        'ipykernel',
-        'pyvinecopulib',
-        'pytest',
-        'mypy'
-    ],
-    license_files=('LICENSE.txt')
-)
+if __name__ == "__main__":
+    setup()
+
+#setup(
+#    name='bivariate',
+#    version='2.0.0',
+#    description='This package contains methods that assist in performing\
+#        bivariate analysis of datasets.',
+#    url='https://github.com/prob-design/bivariate.git',
+#    packages=find_packages(exclude=['old_package*']),
+#    install_requires=[
+#        'numpy',
+#        'pandas',
+#        'matplotlib >= 3.5.0',
+#        'seaborn',
+#        'scipy',
+#        'ipykernel',
+#        'pyvinecopulib',
+#        'pytest',
+#        'mypy'
+#    ],
+#    license_files=('LICENSE.txt')
+#)
