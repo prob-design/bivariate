8f129d28144e10277edabc172cb3e8e855f8c82d
Benjamin Ramousse <b.ramousse@student.tudelft.nl>
2023-09-03 11:32:42 +0200

    Fix calculate_root_LSF

--- src/bivariate/class_multivar.py
+++ src/bivariate/class_multivar.py
@@ -5,54 +5,7 @@ import scipy.stats.distributions
 
 from scipy.optimize import fsolve
 
-import pyvinecopulib as pyc      # Used for sampling: maybe used manually defined classes in the future
-
-from functools import singledispatch, update_wrapper
-
-
-def methdispatch(func):
-    '''
-    Source: https://stackoverflow.com/questions/24601722/how-can-i-use-functools-singledispatch-with-instance-methods
-    For use in pointLSF method (avoids multiple if statements elegantly).
-    '''
-    dispatcher = singledispatch(func)
-
-    def wrapper(*args, **kw):
-        return dispatcher.dispatch(args[3].__class__)(*args, **kw)
-    wrapper.register = dispatcher.register
-    update_wrapper(wrapper, func)
-    return wrapper
-
-def methdispatch(func):
-    '''
-    Source: https://stackoverflow.com/questions/24601722/how-can-i-use-functools-singledispatch-with-instance-methods
-    For use in pointLSF method (avoids multiple if statements elegantly).
-    '''
-    dispatcher = singledispatch(func)
-    def wrapper(*args, **kw):
-        return dispatcher.dispatch(args[3].__class__)(*args, **kw)
-    wrapper.register = dispatcher.register
-    update_wrapper(wrapper, func)
-    return wrapper
-
-# Adapt this to LimitStateFunction class and only have ndarray in second case
-@methdispatch
-def pointLSF(self, myLSF, i, point, start=0):  # For now: supposes failure condition is myLSF() < 0
-    ''' points: int, float, list or 1D ndarray. '''
-    if i == 0:
-        func = lambda y: myLSF([point, y])
-        return fsolve(func, start)[0]
-    elif i == 1:
-        func = lambda x: myLSF([x, point])
-        return fsolve(func, start)[0]
-    else:
-        raise ValueError("Index out of range. Please select i=0 or i=1.")
-
-
-@pointLSF.register(list)
-@pointLSF.register(np.ndarray)
-def _(self, myLSF, i, point, start=0):
-    return [self.pointLSF(myLSF, i, x, start) for x in point]
+import pyvinecopulib as pyc
 
 
 class LimitStateFunction():
@@ -61,7 +14,10 @@ class LimitStateFunction():
 
 
 class Bivariate():
-    def __init__(self, X: list[scipy.stats.distributions.rv_frozen], family, parameter=0):
+    def __init__(self,
+                 X: list,
+                 family,
+                 parameter=0):
         '''
         Define a Bivariate object with three attributes:
 
@@ -72,18 +28,22 @@ class Bivariate():
         self.X = X
         self.family = family
         if family == "Normal":
-            self.copula = pyc.Bicop(family=pyc.BicopFamily.gaussian, parameters=[parameter])
+            self.copula = pyc.Bicop(family=pyc.BicopFamily.gaussian,
+                                    parameters=[parameter])
         elif family == "Clayton":
-            self.copula = pyc.Bicop(family=pyc.BicopFamily.clayton, parameters=[parameter])
+            self.copula = pyc.Bicop(family=pyc.BicopFamily.clayton,
+                                    parameters=[parameter])
         elif family == "Independent":
             self.copula = pyc.Bicop(family=pyc.BicopFamily.indep)
         else:
-            raise ValueError("Invalid copula. Please choose between Normal, Clayton or Independent copulae. ")
+            raise ValueError(
+                "Invalid copula. Please choose between Normal, Clayton or Independent copulae. "
+            )
 
-    def getMarginal(self, i: int)-> scipy.stats.distributions.rv_frozen:
+    def getMarginal(self, i: int) -> scipy.stats.distributions.rv_frozen:
         '''
         Returns the marginal distribution of variable X_i.
-        
+
         i: int. Index of the marginal distribution returned (0 or 1).
         '''
         try:
@@ -100,16 +60,17 @@ class Bivariate():
         i: int. Index of the marginal distribution plotted (0 or 1).
         '''
 
-        f, ax = plt.subplots(figsize=(12, 8))
+        f, ax = plt.subplots(figsize=(10, 6))
 
         var = self.getMarginal(i)
         x = np.linspace(var.ppf(0.01), var.ppf(0.99), 1000)
         y = var.pdf(x)
 
         ax.plot(x, y, label="pdf")
-        ax.set_title(r"Probability density function of $X_{" + str(i) + "}$", fontsize=18)
-        ax.set_xlabel(r"$x_" + str(i) + "}$")
-        ax.set_ylabel(r"f($x_" + str(i) + "})$")
+        ax.set_title(r"Probability density function of $X_{" + str(i) + "}$",
+                     fontsize=18)
+        ax.set_xlabel(r"$x_" + str(i) + "$")
+        ax.set_ylabel(r"f($x_" + str(i) + ")$")
         return f, ax
 
     def drawMarginalCdf(self, i: int):
@@ -119,16 +80,17 @@ class Bivariate():
         i: int. Index of the marginal distribution plotted (0 or 1).
         '''
 
-        f, ax = plt.subplots(figsize=(12, 8))
+        f, ax = plt.subplots(figsize=(10, 6))
 
         var = self.getMarginal(i)
         x = np.linspace(var.ppf(0.01), var.ppf(0.99), 1000)
         y = var.cdf(x)
 
         ax.plot(x, y, label="cdf")
-        ax.set_title(r"Cumulative density function of $X_{" + str(i) + "}$", fontsize=18)
-        ax.set_xlabel(r"$x_" + str(i) + "}$")
-        ax.set_xlabel(r"$F(x_" + str(i) + "})$")
+        ax.set_title(r"Cumulative density function of $X_{" + str(i) + "}$",
+                     fontsize=18)
+        ax.set_xlabel(r"$x_" + str(i) + "$", fontsize=15)
+        ax.set_ylabel(r"$F(x_" + str(i) + ")$", fontsize=15)
         return f, ax
 
     def bivariatePdf(self, x: list[float]) -> float:
@@ -157,10 +119,11 @@ class Bivariate():
         cdf = float(self.copula.cdf([[u0, u1]]))
         return cdf
 
-
 # See how the methods below are affected by the creation of the LimitStateFunction class
+# Robustness of fsolve: some start values lead to converge at wrong values (e.g. start=0 and quadratic functions)
     @staticmethod
-    def calculate_root_LSF(myLSF, i: int, values: list[float], start:float = 0, max_attempts:int = 5) -> float:
+    def calculate_root_LSF(myLSF, i: int, values: list[float],
+                           start: float = 0, max_attempts: int = 5) -> float:
         '''
         Calculates the value of X_i that cancels myLSF given the values in values.
 
@@ -176,10 +139,10 @@ class Bivariate():
         '''
         # Define a temporary function with the missing variable as argument
         def temp_func(x):
-            return myLSF(values[:i] + [x] + values[i+1:])
+            return myLSF(values[:i] + [x] + values[i:])
 
-        # For certain (non-monotonic) functions, inappropriate start_values of fsolve lead to RuntimeWarning
-        # and no convergence.
+        # For certain (non-monotonic) functions, inappropriate start_values of fsolve lead
+        # to RuntimeWarning and no convergence.
         for _ in range(max_attempts):
             start_value = start
             try:
@@ -192,7 +155,8 @@ class Bivariate():
             'An exception has occurred. Please reiterate with a different value of start.'
         )
 
-    def plot_contour(self, ax=None, xlim=None, ylim=None, x_index: int = 0, nb_points=200):
+    def plot_contour(self, ax=None, xlim=None, ylim=None,
+                     x_index: int = 0, nb_points=200):
         '''
         Plots the contour of the probability density function in the plane (X_{x_index}, X_{1-x_index}).
 
@@ -223,17 +187,17 @@ class Bivariate():
 
         x = np.linspace(xlim[0], xlim[1], nb_points).reshape(-1, 1)
         y = np.linspace(ylim[0], ylim[1], nb_points).reshape(-1, 1)
-        X, Y = np.meshgrid(x,y)
+        X, Y = np.meshgrid(x, y)
 
         pdf = np.zeros(X.shape)
         if x_index == 0:
             for i in range(X.shape[0]):
                 for j in range(X.shape[1]):
-                    pdf[i,j] = self.bivariatePdf([X[i, j], Y[i, j]])
+                    pdf[i, j] = self.bivariatePdf([X[i, j], Y[i, j]])
         else:
             for i in range(X.shape[0]):
                 for j in range(X.shape[1]):
-                    pdf[i,j] = self.bivariatePdf([Y[i, j], X[i, j]])
+                    pdf[i, j] = self.bivariatePdf([Y[i, j], X[i, j]])
 
         ax.contour(X, Y, pdf, levels=8, cmap=cm.Blues)
         ax.set_aspect("equal")    # ensures scales of both axis are the same (interpretation of the contours shapes)
@@ -243,78 +207,109 @@ class Bivariate():
         ax.set_ylabel(r"$Y$")
         return f, ax
 
-def plotLSF(self, myLSF, ax=None, xlim:tuple[float]=None, ylim:tuple[float]=None, x_index: int = 0):
-    '''
-    Plots the limit state function in the 2D plane (X_{x_index}, X_{1-x_index}).
 
-    myLSF: function. Limit state function.
-    ax: matplotlib.axes.Axes. Passed if the limit state function is plotted on an existing Axes object.
-    xlim, ylim: tuples. Limits on the x- and y-axes of the plot.
-    x_index: int. Defines which variable is set on the x-axis.
-    '''
-    rv_x = self.X[x_index]
+    def plotLSF(self, myLSF, ax=None, xlim=None, ylim=None, x_index=0):
+        '''
+        Plots the limit state function in the 2D plane (X_{x_index}, X_{1-x_index}).
 
-    if xlim is None:
+        myLSF: function. Limit state function.
+        ax: matplotlib.axes.Axes. Passed if the limit state function is plotted on an existing Axes object.
+        xlim, ylim: tuple[float]. Limits on the x- and y-axes of the plot.
+        x_index: int. Defines which variable is set on the x-axis.
+        '''
+        rv_x = self.X[x_index]
+
+        if xlim is None:
+            if ax is None:
+                xlim = (rv_x.ppf(0.01), rv_x.ppf(0.99))
+            else:
+                xlim = ax.get_xlim()
+        if ylim is None and ax is not None:
+            ylim = ax.get_ylim()
+
         if ax is None:
-            xlim = (rv_x.ppf(0.01), rv_x.ppf(0.99))
+            f, ax = plt.subplots(figsize=(10, 8))
         else:
-            xlim = ax.get_xlim()
-    if ylim is None and ax is not None:
-        ylim = ax.get_ylim()
+            f = plt.gcf()
 
-    if ax is None:
-        f, ax = plt.subplots(figsize=(12,8))
-    else:
-        f = plt.gcf()
+        x = np.linspace(xlim[0], xlim[1], 1000)
+        # Calculate the root of the limit state function for all points of the x-axis
+        y = [self.calculate_root_LSF(myLSF, i=1-x_index, values=[k]) for k in x]
 
-    x = np.linspace(xlim[0], xlim[1], 1000)
-    # Calculate the root of the limit state function for all points of the x-axis
-    y = [self.calculate_root_LSF(myLSF, i= 1-x_index, values=[k]) for k in x]
+        ax.plot(x, y, label="LSF", color="r")
+        ax.set_xlim(xlim)
+        ax.set_title('Limit state function in 2D plane.', fontsize=18)
+        ax.set_xlabel('$x_' + str(x_index) + '$', fontsize=15)
+        ax.set_ylabel('$x_' + str(1 - x_index) + '$', fontsize=15)
 
-    ax.plot(x, y, label="LSF", color="r")
-    ax.set_xlim(xlim)
-    # Fix y-limits if they exist
-    try:
-        ax.set_ylim(ylim)
-    except:
-        pass
+        # Fix y-limits if they exist
+        try:
+            ax.set_ylim(ylim)
+        except:
+            pass
 
-    return f, ax
+        # Add shading of the failure region (myLSF<0 or else)
 
+        return f, ax
+
+
+# Functions to sample copula of X0 and X2 in Multivariate class
 def sampling_cop(cop1, cop2, cond_cop=pyc.Bicop(), n=10000):
-    ''' 
-    cop1: copula between variables X0 and X1.
-    cop2: copula between variables X1 and X2.
-    Assumption: conditional copula (default: independent copula).
-    
+    '''
+    Generates a random sample of the bivariate copula of X0 and X2.
+
+    cop1: pyvinecopulib.Bicop. Copula between variables X0 and X1.
+    cop2: pyvinecopulib.Bicop. Copula between variables X1 and X2.
+    cond_cop: pyvinecopulib.Bicop. Conditional copula between X0 and X2 given X1.
+    (default: independent copula).
+    n: int. Number of samples.
+
     ---------------
     See sampling procedure in http://dx.doi.org/10.1016/j.insmatheco.2007.02.001
     '''
     u = np.random.rand(n, 3)
-    x0 = u[:,0]
-    x1 = cop1.hinv1(np.concatenate((x0.reshape(-1,1), u[:,1].reshape(-1,1)), axis=1))
-    a = cop1.hfunc1(np.concatenate((x0.reshape(-1,1), x1.reshape(-1,1)), axis=1))
-    b = cond_cop.hinv1(np.concatenate((a.reshape(-1,1), u[:,2].reshape(-1,1)), axis=1))
-    x2 = cop2.hinv1(np.concatenate((x1.reshape(-1,1), b.reshape(-1,1)), axis=1))
-    # x = np.concatenate((X0.ppf(x0).reshape(-1,1), X1.ppf(x1).reshape(-1,1), X2.ppf(x2).reshape(-1,1)), axis=1)
-    x = np.concatenate((x0.reshape(-1,1), x1.reshape(-1,1), x2.reshape(-1,1)), axis=1)
+    x0 = u[:, 0]
+    x1 = cop1.hinv1(np.concatenate((x0.reshape(-1, 1),
+                                    u[:, 1].reshape(-1, 1)), axis=1))
+    a = cop1.hfunc1(np.concatenate((x0.reshape(-1, 1),
+                                    x1.reshape(-1, 1)), axis=1))
+    b = cond_cop.hinv1(np.concatenate((a.reshape(-1, 1),
+                                       u[:, 2].reshape(-1, 1)), axis=1))
+    x2 = cop2.hinv1(np.concatenate((x1.reshape(-1, 1),
+                                    b.reshape(-1, 1)), axis=1))
+    x = np.concatenate((x0.reshape(-1, 1),
+                        x1.reshape(-1, 1), x2.reshape(-1, 1)), axis=1)
     return x
 
 
 def fit_copula(x, y, family=pyc.BicopFamily.gaussian):
+    '''
+    Fits a bivariate copula to a sample of random variables X and Y.
+
+    x: ndarray. Sample of variable X.
+    y: ndarray. Sample of variable Y.
+    family: pyvinecopulib.BicopFamily. Family of the copula to fit.
+    '''
+
     fitted_cop = pyc.Bicop(family=family)
     fitted_cop.fit(np.concatenate((x.reshape(-1,1), y.reshape(-1,1)), axis=1))
     return fitted_cop
 
 
 class Multivariate():
-    def __init__(self, X:list, copulas:list):
-        ''' X: vector of random variables. 
-        copula: vector of copulas including the conditional one. '''
-        self.X = X
+    def __init__(self, rv: list, copulas: list):
+        '''
+        Multivariate object for multidimensional probabilistic analyses.
+
+        rv: list. Vector of random variables.
+        copulas: list. Vector of copulas [cop1, cop2, cop_cond] such that
+        cop1 is the copula of rv[0] and rv[1], cop2 of rv[1] and rv[2] and
+        cond_cop the conditional copula of rv[0], rv[2] given rv[1].
+        '''
+        self.rv = rv
         self.copulas = copulas        
-        self.B1 = Bivariate(X[0], X[1], copulas[0][0], copulas[0][1])
-        self.B2 = Bivariate(X[1], X[2], copulas[1][0], copulas[1][1])
+        self.B1 = Bivariate(rv[0], rv[1], copulas[0][0], copulas[0][1])
+        self.B2 = Bivariate(rv[1], rv[2], copulas[1][0], copulas[1][1])
         if self.B1.family == "Normal" and self.B2.family == "Normal" and copulas[2][0] == "Normal":
             family=pyc.BicopFamily.gaussian
             x = sampling_cop(self.B1.copula, self.B2.copula, 
